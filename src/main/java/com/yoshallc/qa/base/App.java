3af4bcff85770ec13906bfdc4ce6e0389ae2e03b
Palakpari Patel <palakparipatel@gmail.com>
2020-01-25 13:33:49 -0500

    initial checkin:

---
+++ src/main/java/com/yoshallc/qa/base/App.java
@@ -0,0 +1,13 @@
+package com.yoshallc.qa.base;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
